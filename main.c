52fa58f635d0495fd722c61050257c4c10092b67
Aditya @ WSL-Ubuntu <adityapi3.14@gmail.com>
2023-09-04 21:21:38 +0530

    Learning about memory leaks. Long way to go.

--- main.c
+++ main.c
@@ -10,29 +10,33 @@ int main(int ac, char **argv){
         printf("%s", prompt);
         ssize_t line_size = getline(&lineptr, &len, stdin);
         if (line_size == -1){
+            free(lineptr);
             printf("\nExiting PiShell\n");
             break;
         }
         char *saveptr;
         char *p = strtok_r(lineptr, delim, &saveptr);
-        char **arr = NULL;
+        char **arr = malloc(80);
         int n_spaces = 0;
         while(p){
             n_spaces++;
-            arr = realloc(arr, sizeof(char*) * n_spaces);
-            if (arr == NULL){
-                exit(EXIT_FAILURE);
-            }
+            // arr = realloc(arr, sizeof(char*) * n_spaces);
+            // if (arr == NULL){
+            //     exit(EXIT_FAILURE);
+            // }
+            arr[n_spaces-1] = malloc(sizeof(char*));
             arr[n_spaces-1] = p;
 
             p = strtok_r(NULL, delim, &saveptr);
         }
-        arr = realloc(arr, sizeof(char*) * n_spaces);
+        // arr = realloc(arr, sizeof(char*) * n_spaces);
+        arr[n_spaces] = malloc(sizeof(char*));
         arr[n_spaces] = NULL;
 
         for (int i = 0; i < n_spaces; i++){
             printf("%s\n", arr[i]);
         }
+        free(arr);
     }
 
     exit(EXIT_SUCCESS);
